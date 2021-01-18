8945b94570f609d93e185bc44caed5d72df664f7
kuba <kubakaszyk@gmail.com>
2021-01-18 21:12:37 +0000

    updated single core example

--- examples/example_single_core.py
+++ examples/example_single_core.py
@@ -4,35 +4,34 @@ from cache import Cache
 from memory import Memory
 
 sys = MemSys("Test Platform", 64)
-ee = ComputeUnit(sys, True)
-l1c = Cache(sys, 0, 6, 16384, 64, 1, True)
-l2c = Cache(sys, 1, 6, 534288, 64, 3, True)
-caches = [l1c, l2c]
-memory = Memory(sys, 10, True)
+memory = Memory(sys, 10, True, None)
+l2c = Cache(sys, 0, 1, 16, 534288, 64, 3, True, memory.get_component_id())
+l1c0 = Cache(sys, 0, 0, 16, 16384, 64, 1, True, l2c.get_component_id())
+cu0 = ComputeUnit(sys, 0, True, l1c0.get_component_id())
+sys.build_map()
+print(sys.component_map)
 
-sys.set_hierarchy([ee,l1c,l2c,memory])
-
-ee.load(0x1000)
-
-i = 0
-while i < 5:
-    sys.advance(1)
-    i += 1
-
-ee.load(0x1000)
-i = 0
-while i < 5:
-    sys.advance(1)
-    i += 1
-
-ee.load(0x1001)
+cu0.load(0x1000)
 
 i = 0
 while i < 5:
     sys.advance(1)
     i += 1
 
-ee.load(0x1002)
+cu0.load(0x1000)
+i = 0
+while i < 5:
+    sys.advance(1)
+    i += 1
+
+cu0.load(0x1001)
+
+i = 0
+while i < 5:
+    sys.advance(1)
+    i += 1
+
+cu0.load(0x1002)
 i = 0
 while i < 5:
     sys.advance(1)
