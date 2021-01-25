3cc7191389a858dccef79005a50eb234157691d7
kuba <kubakaszyk@gmail.com>
2021-01-25 10:50:17 +0000

    memory bandwidth model

--- examples/example_single_core.py
+++ examples/example_single_core.py
@@ -4,7 +4,7 @@ from cache import Cache
 from memory import Memory
 
 sys = MemSys("Test Platform", 64)
-memory = Memory(sys, 10, 2, 64, 1866, True, None)
+memory = Memory(sys, 4, 4, 2, 64, 1866, True, None)
 l2c = Cache(sys, 1037000, 0, 1, 16, 534288, 64, 3, True, memory.get_component_id())
 l1c0 = Cache(sys, 1037000, 0, 0, 16, 16384, 64, 1, True, l2c.get_component_id())
 cu0 = ComputeUnit(sys, 1037000, 0, True, l1c0.get_component_id())
--- sim/memory.py
+++ sim/memory.py
@@ -3,16 +3,17 @@ from util import Logger
 from mem_sys import MemSysComponent
 
 class Memory(MemSysComponent):
-    def __init__(self, sys, latency, tfrs_per_clk, bit_width, clk_speed, logger_on, lower_component_id):
+    def __init__(self, sys, latency, max_parallel_reads, tfrs_per_clk, bit_width, clk_speed, logger_on, lower_component_id):
         super().__init__("Memory", clk_speed, sys, lower_component_id)
         self.mem_queue = []
         self.latency = latency
         self.logger = Logger(self.name, logger_on, self.mem_sys)
 
+        self.max_parallel_reads = max_parallel_reads
         self.tfrs_per_clk = tfrs_per_clk
         self.bit_width = bit_width
         self.clk_speed = clk_speed #MHz
-        self.clk = 0 
+        self.clk = 0
 
     def print_bandwidth(self):
         bandwidth = self.clk_speed * self.tfrs_per_clk * self.bit_width
@@ -31,11 +32,12 @@ class Memory(MemSysComponent):
         remove_list = []
         address_list = []
         
-        for i in range(len(self.mem_queue)):
-            self.mem_queue[i][1] = self.mem_queue[i][1] - cycles
-            if self.mem_queue[i][1] <= 0:
-                self.return_load(self.mem_queue[i][0])
-                remove_list.append(i)
+        for i in range(self.max_parallel_reads):
+            if i < len(self.mem_queue):
+                self.mem_queue[i][1] = self.mem_queue[i][1] - cycles
+                if self.mem_queue[i][1] <= 0:
+                    self.return_load(self.mem_queue[i][0])
+                    remove_list.append(i)
 
         print(self.mem_queue)
         remove_list.reverse()
